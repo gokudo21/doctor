de2555249a66600bcc12c36caefa1f873108b002
gokudo21 <gokudo21@gmail.com>
2018-07-14 15:15:27 +0300

    update host and port

--- main.py
+++ main.py
@@ -123,4 +123,9 @@ def edit2():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        app.run(debug=True, host='0.0.0.0', port=80)
+    except Exception as e:
+        print("exception: ")
+        print(str(e))
+        app.run(debug=True)
